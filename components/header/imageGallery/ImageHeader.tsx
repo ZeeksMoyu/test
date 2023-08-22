5fa40fe040a9133ae029611889cb633b30cae180
Arejim <zeeksmoyu@gmail.com>
2023-08-22 23:35:55 +0200

    first commit

---
+++ components/header/imageGallery/ImageHeader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Image.module.css'
+
+const Image = () => {
+    return (
+        <div className={classes.headerAd}>
+            
+        </div>
+    );
+};
+
+export default Image;
